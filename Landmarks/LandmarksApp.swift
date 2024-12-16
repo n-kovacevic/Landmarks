37f4522863e45b81620d10c012c7cb8bc254c675
Nikola Kovacevic <contact@nkovacevic.com>
2024-12-16 22:51:13 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nikola Kovacevic on 16.12.24..
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
