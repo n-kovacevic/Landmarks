3cbe9bbc4e4ab8abf5dd6f8aa53b6732f8de2182
Nikola Kovacevic <contact@nkovacevic.com>
2024-12-17 09:16:51 +0100

    Added option to filter landmarks based on favorite

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List (landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                ForEach(filteredLandmarks) {landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
+            .animation(.default, value: filteredLandmarks)
         } detail: {
                 Text("Select a Landmark")
         }
